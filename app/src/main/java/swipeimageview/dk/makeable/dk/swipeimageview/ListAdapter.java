eb840e1cef25fa4f51269693f0f332cf0393b77b
Maximilian Bosch <max.bosch@outlook.com>
2016-11-19 20:17:13 +0100

    Implement swipe to reveal functionality

--- app/src/main/java/swipeimageview/dk/makeable/dk/swipeimageview/ListAdapter.java
+++ app/src/main/java/swipeimageview/dk/makeable/dk/swipeimageview/ListAdapter.java
@@ -1,11 +1,15 @@
 package swipeimageview.dk.makeable.dk.swipeimageview;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.BaseSwipeAdapter;
 
 import java.util.ArrayList;
 
@@ -13,7 +17,7 @@ import java.util.ArrayList;
  * Created by simonchristensen on 18/11/2016.
  */
 
-public class ListAdapter extends BaseAdapter{
+public class ListAdapter extends BaseSwipeAdapter {
 
     private Context mContext;
     private ArrayList<String> mImageViews;
@@ -35,25 +39,64 @@ public class ListAdapter extends BaseAdapter{
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
-    public View getView(int i, View convertView, ViewGroup viewGroup) {
-        final ViewHolder holder;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
-            holder = new ViewHolder();
-            holder.mImageView = (ImageView) convertView.findViewById(R.id.imageView);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        return convertView;
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe_layout;
     }
 
-    private static class ViewHolder{
-        private ImageView mImageView;
+    @Override
+    public View generateView(int position, ViewGroup parent) {
+        return LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
+    }
+
+    @Override
+    public void fillValues(int position, View convertView) {
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.image_view);
+        imageView.setBackgroundColor(Color.rgb(position * 100 % 256, 255, 255));
+
+        TextView textView = (TextView) convertView.findViewById(R.id.text_view);
+        textView.setText(String.format("This is the content at Position %d.", position));
+
+        View swipeView = convertView.findViewById(R.id.swipe_view);
+        swipeView.setBackgroundColor(Color.rgb(position * 100 % 256, 0, 0));
+
+        // Set up swipe layout & event listener
+        SwipeLayout swipeLayout = (SwipeLayout) convertView.findViewById(R.id.swipe_layout);
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
     }
 }
--- app/src/main/java/swipeimageview/dk/makeable/dk/swipeimageview/MainActivity.java
+++ app/src/main/java/swipeimageview/dk/makeable/dk/swipeimageview/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import com.daimajia.swipe.SwipeLayout;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
